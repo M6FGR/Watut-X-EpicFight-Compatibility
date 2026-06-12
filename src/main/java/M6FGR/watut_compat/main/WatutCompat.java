603de6118f1eabf38cb88ad5ea529b1cac88dd74
M6FGR <azooz77899@gmail.com>
2026-06-12 17:28:43 +0300

    First Push

---
+++ src/main/java/M6FGR/watut_compat/main/WatutCompat.java
@@ -0,0 +1,20 @@
+package M6FGR.watut_compat.main;
+
+import M6FGR.watut_compat.gameassets.WatutAnimations;
+import com.corosus.watut.WatutMod;
+import com.mojang.logging.LogUtils;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.loading.FMLLoader;
+import org.slf4j.Logger;
+
+@Mod(WatutCompat.MODID)
+public class WatutCompat {
+    public static final String MODID = "watut_compat";
+    public WatutCompat(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(WatutAnimations::registerAnimations);
+
+    }
+
+}
